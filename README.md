3640aad81d26bdd53b462083d6526178a1d7e39a
Ryan Galvin <ryanjamesgalvin@gmail.com>
2018-07-10 00:20:25 -0400

    Second commit for logs analysis project

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ a PostgreSQL database.
 Clone or download the files to your computer, then in your terminal inside the virtual machine
 (with current directory to the project.py location), type
 
+`psql`
+
+then type
+
+`CREATE VIEW error_log_view AS SELECT date(TIME), round(100.0 * sum(CASE LOG.status WHEN '200 OK' THEN 0 ELSE 1 END) / count(LOG.status), 2) AS "Percent Error" FROM log GROUP BY date(TIME) ORDER BY "Percent Error" DESC;`
+
+to create the SQL view necessary for the third query. Reopen the virtual machine and type:
+
 `python project.py`
 
 to run the program. You should see the programs output in the terminal.
--- project.py
+++ project.py
@@ -1,46 +1,35 @@
+#!/usr/bin/env python3
 import psycopg2
 conn = psycopg2.connect(dbname="news")
 cur = conn.cursor()
-cur.execute("""SELECT a.slug, count(a.slug) as count 
+cur.execute("""SELECT a.title, a.slug, count(a.slug) as count 
                FROM log l JOIN articles a 
                ON l.path = concat('/article/', a.slug) 
-               GROUP BY a.slug 
+               GROUP BY a.slug, a.title 
                ORDER BY count DESC 
                LIMIT 3;""")
-print("What are the most popular three articles of all time?")
+print("What are the most popular three articles of all time?" + "\n")
 for record in cur:
-    print(record)
+    print(record[0] + " -- " + str(record[2]) + " views")
 cur.close()
 cur2 = conn.cursor()
-cur2.execute("""SELECT a.slug, count(a.slug) as count, au.name 
+cur2.execute("""SELECT count(l.id) as count, au.name 
                 FROM articles a JOIN log l ON l.path = concat('/article/', a.slug) 
                 JOIN authors au 
                 ON au.id = a.author 
-                GROUP BY a.slug, au.name 
+                GROUP BY au.name
                 ORDER BY count DESC 
                 LIMIT 3; """)
 print("Who are the most popular article authors of all time? " + "\n")
 for record in cur2:
-    print(record)
+    print(record[1] + " -- " + str(record[0]) + " views")
 cur2.close()
 cur3 = conn.cursor()
-cur3.execute("""WITH query3 AS (WITH query1 as 
-                (select time, status as s1 
-                from log 
-                where status ='404 NOT FOUND'), 
-                query2 as 
-                (select time, status as s2 from log)
-                select  q1.time as time, count(s1) / (count(s2)) as ratio 
-                from query1 as q1 
-                INNER JOIN query2 as q2 
-                on q1.time = q2.time
-                GROUP BY q1.time)
-                SELECT time, ratio
-                FROM query3
-                WHERE ratio > 0.01
-                LIMIT 3;""")
-print("On which days did more than 1% of requests lead to errors?")
+cur3.execute("""SELECT date
+                FROM error_log_view
+                WHERE "Percent Error" > 1""")
+print("On which days did more than 1% of requests lead to errors?" + "\n")
 for record in cur3:
-    print(record)
+    print(record[0])
 cur3.close()
 conn.close()
